b9a2401da74745a4bb729c542cc029615b45d9ac
James Edmonds <james@exhibitionist.digital>
2025-11-07 03:28:06 +0000

    wip

--- packages/core/src/components/FormField.tsx
+++ packages/core/src/components/FormField.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Stack, Text } from '@sanity/ui'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, useMemo } from 'react'
 import { FormFieldValidationStatus, ValidationMarker } from 'sanity'
 
 export default function FormField(
@@ -9,18 +9,24 @@ export default function FormField(
     markers?: ValidationMarker[]
   }>,
 ) {
+  const hasMarkers = useMemo(() => {
+    return props.markers && props.markers.length > 0
+  }, [props.markers])
+
   return (
     <Stack space={3}>
       <Text weight="semibold" size={1}>
         <Flex gap={1} align="center">
           {props.label}
-          <FormFieldValidationStatus
-            validation={props.markers?.map((m) => ({
-              level: m.level,
-              message: m.message || m.item?.message || '',
-              path: m.path,
-            }))}
-          />
+          {hasMarkers && (
+            <FormFieldValidationStatus
+              validation={props.markers?.map((m) => ({
+                level: m.level,
+                message: m.message || m.item?.message || '',
+                path: m.path,
+              }))}
+            />
+          )}
         </Flex>
       </Text>
       {props.description && (
